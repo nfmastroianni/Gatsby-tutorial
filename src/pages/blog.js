a24bf2de084caed39f24a4229669ab5fd55562a5
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2020-12-23 10:21:18 -0500

    adding blog page and navigation component

---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+export default function Blog() {
+  return (
+    <div>
+      <h1>This is the Blog Page</h1>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import Navbar from "../components/navbar"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Navbar />
+    </div>
+  )
 }
