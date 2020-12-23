44b72e7631c644b496722d216206144281fec763
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2020-12-23 13:03:31 -0500

    adding styled components

---
+++ src/components/button.js
@@ -0,0 +1,8 @@
+import React from "react"
+import styled from "styled-components"
+
+export const ExampleButton = styled.button`
+  background: green;
+  color: orange;
+  font-size: 2rem;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
+import { ExampleButton } from "../components/button"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Hello world!</h1>
+      <ExampleButton>Click Me</ExampleButton>
     </Layout>
   )
 }
